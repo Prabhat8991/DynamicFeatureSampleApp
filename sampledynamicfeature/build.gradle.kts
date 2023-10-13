cb812d4808a805f1a53b1304803c5bd76a9a481f
prabhat.singh <prabhat.singh@thoughworks.com>
2023-10-13 15:44:02 +0530

    added dependencies to dynamic feature

--- sampledynamicfeature/build.gradle.kts
+++ sampledynamicfeature/build.gradle.kts
@@ -25,9 +25,7 @@ android {
 dependencies {
 
     val nav_version = "2.3.3"
-
     implementation(project(":app"))
-    implementation("androidx.core:core-ktx:1.9.0")
 
     // Kotlin
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
@@ -35,8 +33,13 @@ dependencies {
 
     // Feature module Support
     implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
+
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.annotation:annotation:1.7.0")
 }
